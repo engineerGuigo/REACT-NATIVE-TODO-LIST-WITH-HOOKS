9c27d71f8c06869872adfa08d41fbd7c7b00c4d0
Rodrigo Durante <rodrigodurante@Guilhermes-MacBook-Pro.local>
2020-02-19 00:39:58 -0300

    Custom Components

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() { 
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   };
 
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: enteredGoal }]);
   };
 
   return (
@@ -22,7 +24,7 @@ export default function App() {
         />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View/>
+      <FlatList keyExtractor={(item, index) => item.id} data={courseGoals} renderItem={itemData => <View style={styles.listItem}><Text>{itemData.item.value}</Text></View>}/>
     </View>
   );
 }
@@ -41,5 +43,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    margin: 10, 
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
